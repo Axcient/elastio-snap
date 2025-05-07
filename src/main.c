b820b6a39841d3e04ab9b69e31042abd2136fc76
user <user@ubuntu-20.04-mbr>
2025-05-06 23:54:55 -0400

    [RA-6218] Additional debug considerations
    Implemented a counter to track REQ_OP_DISCARD I/O that might
    invalidate original disk sectors in the Thin-Provisioning VMs

--- src/main.c
+++ src/main.c
@@ -1164,6 +1164,7 @@ struct snap_device{
 	atomic64_t sd_submitted_cnt; //count of read clones submitted to underlying driver
 	atomic64_t sd_received_cnt; //count of read clones submitted to underlying driver
 	atomic64_t sd_processed_cnt; //count of read clones processed in snap_cow_thread()
+	atomic64_t sd_discard_cnt; //count of discard bio requests
 };
 
 static long ctrl_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
@@ -4489,6 +4490,11 @@ static MRF_RETURN_TYPE tracing_mrf(struct request_queue *q, struct bio *bio){
 			goto call_orig;
 		}
 
+		if (bio_is_discard(bio)) {
+			PRINT_BIO("bio_op_discard", bio);
+			atomic64_inc(&dev->sd_discard_cnt);
+		}
+
 		if(tracer_should_trace_bio(dev, bio)){
 			if(test_bit(SNAPSHOT, &dev->sd_state)) {
 #ifdef NETLINK_DEBUG
@@ -5332,6 +5338,7 @@ static int __tracer_setup_snap(struct snap_device *dev, unsigned int minor, stru
 	atomic64_set(&dev->sd_submitted_cnt, 0);
 	atomic64_set(&dev->sd_received_cnt, 0);
 	atomic64_set(&dev->sd_processed_cnt, 0);
+	atomic64_set(&dev->sd_discard_cnt, 0);
 
 	return 0;
 
@@ -7051,6 +7058,7 @@ static int elastio_snap_proc_show(struct seq_file *m, void *v){
 
 		seq_printf(m, "\t\t\t\"state\": %lu,\n", dev->sd_state);
 		seq_printf(m, "\t\t\t\"ignore_errors\": %i,\n", dev->sd_ignore_snap_errors);
+		seq_printf(m, "\t\t\t\"bio_op_discard\": %llu,\n", atomic64_read(&dev->sd_discard_cnt));
 		seq_printf(m, "\t\t\t\"cow_on_bdev\": %s\n", test_bit(COW_ON_BDEV, &dev->sd_cow_state) ? "true" : "false");
 		seq_printf(m, "\t\t}");
 	}
