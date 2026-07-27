23c0a466024e2b80f92b3f8257990713dde21eaa
Konstantin Germanov <kgermanov@axcient.com>
2026-07-27 11:50:33 +0400

    [RA-7728] Use check_compiler before genconfig.sh

---
+++ src/check_compiler.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-only
+
+SRC_DIR=$(dirname "$0")
+FEATURE_TEST_DIR="$SRC_DIR/configure-tests/feature-tests"
+FEATURE_TEST_FILES="$FEATURE_TEST_DIR/compiler.c"
+KERNEL_VERSION=${1:-$(uname -r)}
+MAKE_ENV_FILE=${2:-"elastio.env"}
+
+ln -sf /dev/null "$MAKE_ENV_FILE"
+if [[ -f /etc/redhat-release ]]; then
+    echo "Check compiler for redhat kernel-${KERNEL_VERSION} ..."
+    rm -rf "${FEATURE_TEST_DIR}/build"
+    make -s -C $FEATURE_TEST_DIR clean KERNELVERSION=$KERNEL_VERSION
+
+    if make -C $FEATURE_TEST_DIR TEST_NAME=compiler KERNELVERSION=$KERNEL_VERSION; then
+        echo "System compiler was passed"
+        exit 0
+    fi
+
+    for toolset in $(ls -1 /opt/rh/gcc-toolset-*/enable); do
+        source ${toolset}
+        if make -C $FEATURE_TEST_DIR TEST_NAME=compiler KERNELVERSION=$KERNEL_VERSION; then
+            echo "Toolset ${toolset} was passed"
+            ln -sf "${toolset}" "$MAKE_ENV_FILE"
+            exit 0
+        fi
+    done
+
+    GCC_MAJOR=$(sed -nE 's/CONFIG_GCC_VERSION=(.+)..../\1/p' "/boot/config-$KERNEL_VERSION")
+    echo "Any toolset was not passed. Most probably need to install gcc-toolset-${GCC_MAJOR}."
+fi
--- src/genconfig.sh
+++ src/genconfig.sh
@@ -2,11 +2,10 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 # ./genconfig.sh - generate C header for setting the correct preprocessor definitions for the kernel version
-#                  generate env file for setting up the required environment before MAKE[#]
-# Usage ./genconfig.sh <kernel version> <env file name> <make flags>
+# Usage ./genconfig.sh <kernel version> <make flags>
 # The make flags are used for determining concurrency for the feature tests, it pulls out the value of the -j flag.
-# ./genconfig.sh `uname -r` "elastio.env" "-j4", four threads for running feature tests
-# ./genconfig.sh `uname -r` "elastio.env" "-i -j4 -d", doesn't care about other flags present
+# ./genconfig.sh `uname -r` "-j4", four threads for running feature tests
+# ./genconfig.sh `uname -r` "-i -j4 -d", doesn't care about other flags present
 
 SRC_DIR=$(dirname "$0")
 OUTPUT_FILE=$SRC_DIR/kernel-config.h
@@ -15,8 +14,7 @@ FEATURE_TEST_FILES="$FEATURE_TEST_DIR/*.c"
 SYMBOL_TESTS_FILE="$SRC_DIR/configure-tests/symbol-tests"
 CONFIG_TESTS_FILE="$SRC_DIR/configure-tests/config-tests"
 KERNEL_VERSION=$(uname -r)
-MAKE_ENV_FILE=${2:-"elastio.env"}
-MAX_THREADS=$(echo "$3" | sed -E 's/.*-j\s*([0-9]+).*/\1/')
+MAX_THREADS=$(echo "$2" | sed -E 's/.*-j\s*([0-9]+).*/\1/')
 if ! [[ "$MAX_THREADS" =~ '^[0-9]+$' ]]; then # if there was no -j flag provided, default to the number of processors
 	MAX_THREADS=$(getconf _NPROCESSORS_ONLN)
 fi
@@ -29,7 +27,7 @@ fi
 # the debug linux kernel package and extract it from there
 deb_extract_system_map() {
 	LINUX_IMAGE_DBG="linux-image-$KERNEL_VERSION-dbg"
-	URL=$(sudo apt-get download --print-uris linux-image-$KERNEL_VERSION-dbg | awk -F\' {'print $3'})
+	URL=$(sudo apt-get download --print-uris linux-image-$KERNEL_VERSION-dbg | awk -F\' {'print $2'})
 	echo "Downloading $LINUX_IMAGE_DBG from $URL..."
 	if ! wget -q "$URL"; then
 		echo "Could not download $LINUX_IMAGE_DBG"
@@ -133,7 +131,7 @@ while read CONFIG_OPTION; do
 
 	echo "checking $CONFIG_OPTION"
 	MACRO_NAME="$(echo ${CONFIG_OPTION} | awk '{print toupper($0)}')"
-	CONFIG_VALUE=$(grep "${CONFIG_OPTION}" "${SYSTEM_CONFIG_FILE}" | awk -F"=" '{print $3}')
+	CONFIG_VALUE=$(grep "${CONFIG_OPTION}" "${SYSTEM_CONFIG_FILE}" | awk -F"=" '{print $2}')
 	if [ -n "$CONFIG_VALUE" ]; then
 		echo "#define $MACRO_NAME $CONFIG_VALUE" >> $OUTPUT_FILE
 	fi
@@ -141,27 +139,3 @@ done < $CONFIG_TESTS_FILE
 
 echo "" >> $OUTPUT_FILE
 echo "#endif" >> $OUTPUT_FILE
-
-configure_gcc_for_dkms_rhel() {
-	local GCC_MAJOR=$(sed -nE 's/CONFIG_GCC_VERSION=(.+)..../\1/p' "$SYSTEM_CONFIG_FILE")
-
-	if [[ -z $GCC_MAJOR ]]; then
-		echo "could not parse GCC major version"
-		return
-	fi
-
-	for ((i=0; i<5; i++)); do
-		local GCC_ENV="/opt/rh/gcc-toolset-$((GCC_MAJOR+i))/enable"
-
-		if [[ -f "$GCC_ENV" ]]; then
-			ln -sf "$GCC_ENV" "$MAKE_ENV_FILE"
-			break
-		fi
-	done
-}
-
-ln -sf /dev/null "$MAKE_ENV_FILE"
-
-if [[ -f /etc/redhat-release ]]; then
-	configure_gcc_for_dkms_rhel
-fi
