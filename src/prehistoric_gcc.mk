1152e9dd317494b139c0f1b21b02fab5b86957f5
Zhora Gevorgyan <zhora.gevorgyan@connectwise.com>
2026-07-27 17:49:04 +0400

    [RA-7728] exlude the flags only if make variable is set

--- src/prehistoric_gcc.mk
+++ src/prehistoric_gcc.mk
@@ -1,8 +1,9 @@
+ifeq ($(COMPAT_OLD_GCC),y)
 UNSUPPORTED_OLD_GCC_FLAGS := \
-    -fzero-call-used-regs=% \
-    -ftrivial-auto-var-init=% \
-    -Wno-alloc-size-larger-than \
-    -Werror=stringop-truncation \
-    -pg
+	-fzero-call-used-regs=% \
+	-ftrivial-auto-var-init=% \
+	-Wno-alloc-size-larger-than \
+	-pg
 KBUILD_CFLAGS := $(filter-out $(UNSUPPORTED_OLD_GCC_FLAGS),$(KBUILD_CFLAGS))
 KBUILD_CFLAGS += -Wno-pragmas -Wno-error=stringop-truncation
+endif
