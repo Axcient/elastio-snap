54122a169f73b43a281fbb89ce53299c4ac51c1c
Konstantin Germanov <kgermanov@axcient.com>
2026-08-05 17:08:07 +0400

    [RA-7728] Do not inject warn ignore even for old conpiler

--- src/prehistoric_gcc.mk
+++ src/prehistoric_gcc.mk
@@ -4,4 +4,4 @@ UNSUPPORTED_OLD_GCC_FLAGS := \
 	-Wno-alloc-size-larger-than \
 	-pg
 KBUILD_CFLAGS := $(filter-out $(UNSUPPORTED_OLD_GCC_FLAGS),$(KBUILD_CFLAGS))
-KBUILD_CFLAGS += -Wno-pragmas -Wno-error=stringop-truncation
+KBUILD_CFLAGS += -Wno-pragmas
