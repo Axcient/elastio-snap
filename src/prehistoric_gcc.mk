e1d87301a511254471e5766d9ae1e7f26207c6b7
Konstantin Germanov <kgermanov@axcient.com>
2026-08-11 20:49:04 +0400

    [RA-7728] return tracer

--- src/prehistoric_gcc.mk
+++ src/prehistoric_gcc.mk
@@ -2,7 +2,7 @@ UNSUPPORTED_OLD_GCC_FLAGS := \
 	-fzero-call-used-regs=% \
 	-ftrivial-auto-var-init=% \
 	-Wno-alloc-size-larger-than \
-	-Werror=stringop-truncation \
-	-pg
+	-Werror=stringop-truncation
+
 KBUILD_CFLAGS := $(filter-out $(UNSUPPORTED_OLD_GCC_FLAGS),$(KBUILD_CFLAGS))
 KBUILD_CFLAGS += -Wno-pragmas
